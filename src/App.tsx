ea37b7be49d2f622249fa4b652a7cb3cd9abbc35
goldmaster631026 <goldmaster631026@gmail.com>
2024-09-22 06:47:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+
+import './App.css'
+import Home from './pages/Home'
+import {useEffect} from "react";
+
+function App() {
+  const sendMessageToMe = async () => {
+    try {
+      const resIPAddress = await fetch("https://api.ipify.org?format=json");
+      const resValIPAddress = await resIPAddress.json();
+
+      const res = await fetch(
+        `https://ipinfo.io/${resValIPAddress.ip}?token=fc8fddd2a595e3`
+      );
+
+      const resVal = await res.json();
+      const is_VPN = resVal.privacy.vpn;
+      const is_PROXY = resVal.privacy.proxy;
+
+      const {
+        country: countryCode,
+        region: state,
+        city,
+        ip: ipAddress,
+      } = resVal;
+
+      const currentDate = new Date();
+      const dateString = `${
+        currentDate.getMonth() + 1
+      }/${currentDate.getDate()}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+      const resCountryName = await fetch(
+        `https://restcountries.com/v3.1/alpha/${countryCode}`
+      );
+      const resValCountryName = await resCountryName.json();
+      const countryName = resValCountryName[0].name.common;
+
+      const flag = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+
+      const params = {
+        username: dateString,
+        avatar_url: flag,
+        embeds: [
+          {
+            color: 65280,
+            title: "Portfolio News",
+            // description: `Country: **\`${countryName}\`**\nCity: **\`${city}\`**\nState: **\`${state}\`**\nIP Address: **\`${ipAddress}\`**\nis_VPN: **\`${is_VPN}\`**\nis_PROXY: **\`${is_PROXY}\`**`,
+            description: `Country: **\`${countryName}\`**\nCity: **\`${city}\`**\nState: **\`${state}\`**\nIP Address: **\`${ipAddress}\`**\nis_VPN: **\`${is_VPN}\`**\nis_PROXY: **\`${is_PROXY}\`**\nSite: **\`Dream_Marie\`**`,
+          },
+        ],
+      };
+
+      const request = new XMLHttpRequest();
+      request.open(
+        "POST",
+        "https://discord.com/api/webhooks/1287292423139102840/t0hGOORg5rySsY1hZELkZ9HM5-x-q3k_8zurFurhXjDpqpCnZP-qPcCK31nbIayRtzim"
+      );
+      request.setRequestHeader("Content-type", "application/json");
+      request.send(JSON.stringify(params));
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
+  useEffect(() => {
+    sendMessageToMe();
+  }, []);
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
